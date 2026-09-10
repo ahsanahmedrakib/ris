9fea346a14b6655624f6c4a6abd1944fb80f8eda
ahsan.rakib <ahsan.rakib@technonext.com>
2026-09-10 18:01:14 +0600

    clever: add config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Livewire::forceAssetInjection();
+
+        if (app()->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
